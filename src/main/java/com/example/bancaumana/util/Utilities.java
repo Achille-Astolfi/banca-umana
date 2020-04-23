58dc470f9a2cff780e369c97bd03c46093b6c53c
Achille Astolfi <astolfia@example.com>
2020-04-23 17:04:54 +0200

    Skeleton Utilities

---
+++ src/main/java/com/example/bancaumana/util/Utilities.java
@@ -0,0 +1,12 @@
+package com.example.bancaumana.util;
+
+import java.util.Date;
+
+public class Utilities {
+	private Utilities() {}
+	
+	public static Date parseDatabaseDate(String dataDb) {
+		// TODO
+		return null;
+	}
+}
