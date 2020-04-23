c336116986f828aa6033cceb84ab17feae0bf4d5
Petrut Cabulca <Petrut Cabulca petrut.cabulca@example.com>
2020-04-23 17:13:21 +0200

    date

--- src/main/java/com/example/bancaumana/mapper/StoricoRowMapper.java
+++ src/main/java/com/example/bancaumana/mapper/StoricoRowMapper.java
@@ -14,9 +14,16 @@ public class StoricoRowMapper implements RowMapper<Storico> {
 	@Override
 	public Storico mapRow(ResultSet rs, int rowNum) throws SQLException {
 		Storico storico = new Storico();
-		// BEGIN set dei valori
+
+		storico.setnConto(rs.getString("sto_numeroconto"));
+		storico.setImpMovimento(rs.getBigDecimal("sto_importomovimento"));
+		storico.setDataMovimento(rs.getString("sto_datamovimento"));
+		storico.setOraMovimento(rs.getInt("sto_oramovimento"));
+		storico.setDescrizione(rs.getString("sto_descrizione"));
+		storico.setCausale(rs.getString("sto_causale"));
 		
-		// END set dei valori
+		
+		
 		return storico;
 	}
 	
