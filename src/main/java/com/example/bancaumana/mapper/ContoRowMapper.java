48acaa5e29a2c730525a90d3dd5e29f65ebdd5ea
Jacopo Foti <13jacopo@gmail.com>
2020-04-23 16:42:15 +0200

    .

--- src/main/java/com/example/bancaumana/mapper/ContoRowMapper.java
+++ src/main/java/com/example/bancaumana/mapper/ContoRowMapper.java
@@ -1,7 +1,10 @@
 package com.example.bancaumana.mapper;
 
+import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.jdbc.core.RowMapper;
 
@@ -9,13 +12,17 @@ import com.example.bancaumana.entity.Conto;
 
 public class ContoRowMapper implements RowMapper<Conto> {
 
+		
 	@Override
 	public Conto mapRow(ResultSet rs, int rowNum) throws SQLException {
-		Conto conto = new Conto();
-		// BEGIN set dei valori
-		
-		// END set dei valori
-		return conto;
+
+		Conto conto = new Conto();		
+		for (rowNum = 0; true; rowNum++) {		
+		conto.setnConto(rs.getString("con_numeroconto"));
+		conto.setValuta(rs.getString("con_divisa"));
+		conto.setFido(rs.getBigDecimal("con_fido"));
+		}
+
 	}
 
 }
