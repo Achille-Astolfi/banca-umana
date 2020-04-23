0200014baf0132ea6917ce2e6ef522a0b5c65bc0
l_das <lauradealmeidasouza123@gmail.com>
2020-04-23 17:11:00 +0200

    MovimentoMapper senza conversione data

---
+++ src/main/java/com/example/bancaumana/mapper/MovimentoRowMapper.java
@@ -0,0 +1,35 @@
+package com.example.bancaumana.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.example.bancaumana.entity.Movimento;
+/**
+ * 
+ * @author Laura
+ *
+ */
+
+public class MovimentoRowMapper implements RowMapper<Movimento>{
+
+	@Override
+	public Movimento mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Movimento movimento = new Movimento();
+		
+		movimento.setnConto(rs.getString("mov_numeroconto"));
+		movimento.setImpMovimento(rs.getBigDecimal("mov_importomovimenti"));
+		movimento.setDataMovimento(rs.getDate("mov_datamovimento"));
+		movimento.setOraMovimento(rs.getInt("mov_oramovimento"));
+		movimento.setDescrizione(rs.getString("mov_desmovimento"));
+		movimento.setStato(rs.getInt("mov_stato"));
+		movimento.setCausale(rs.getString("mov_causale"));
+		
+		
+		return movimento;
+	}
+	
+	
+
+}
