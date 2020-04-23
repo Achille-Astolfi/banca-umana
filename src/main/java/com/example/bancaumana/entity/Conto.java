ca2ad01e3dbe2971d9f7023251354b549e181a84
l_das <lauradealmeidasouza123@gmail.com>
2020-04-23 15:27:59 +0200

    ContoVo

--- src/main/java/com/example/bancaumana/entity/Conto.java
+++ src/main/java/com/example/bancaumana/entity/Conto.java
@@ -1,5 +1,34 @@
 package com.example.bancaumana.entity;
 
+import java.math.BigDecimal;
+
 public class Conto {
+	
+	private String nConto;
+	private String valuta;
+	private BigDecimal fido;
+	
+	//GETTER AND SETTER
+	
+	public String getnConto() {
+		return nConto;
+	}
+	public void setnConto(String nConto) {
+		this.nConto = nConto;
+	}
+	public String getValuta() {
+		return valuta;
+	}
+	public void setValuta(String valuta) {
+		this.valuta = valuta;
+	}
+	public BigDecimal getFido() {
+		return fido;
+	}
+	public void setFido(BigDecimal fido) {
+		this.fido = fido;
+	}
+	
+	
 
 }
