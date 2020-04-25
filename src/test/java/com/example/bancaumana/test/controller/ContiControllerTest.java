55f0564cff0fcb692048fffeecfdeeb2f9f4a0a3
Achille Astolfi <astolfia@example.com>
2020-04-25 19:27:43 +0200

    test

---
+++ src/test/java/com/example/bancaumana/test/controller/ContiControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.bancaumana.test.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.bancaumana.boot.BancaUmanaBoot;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BancaUmanaBoot.class)
+@AutoConfigureMockMvc
+public class ContiControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void getContoTest() throws Exception {
+		mockMvc.perform(get("/conti/310020200001")//
+				.accept(MediaType.APPLICATION_JSON))//
+				.andExpect(status().isOk())//
+				.andExpect(jsonPath("$._links.self").exists());
+	}
+}
