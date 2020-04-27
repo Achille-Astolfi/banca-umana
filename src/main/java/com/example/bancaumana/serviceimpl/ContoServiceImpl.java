d6ab592fa894a2b43112dd889e33823c22ed12ca
Jacopo Foti <13jacopo@gmail.com>
2020-04-27 15:51:42 +0200

    .

--- src/main/java/com/example/bancaumana/serviceimpl/ContoServiceImpl.java
+++ src/main/java/com/example/bancaumana/serviceimpl/ContoServiceImpl.java
@@ -1,11 +1,21 @@
 package com.example.bancaumana.serviceimpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.bancaumana.model.ContoModel;
+import com.example.bancaumana.repoimpl.ContiRepoImpl;
+import com.example.bancaumana.repoimpl.MovimentiRepoImpl;
+import com.example.bancaumana.repoimpl.SaldiRepoImpl;
 
 @Service
 public class ContoServiceImpl {
+	@Autowired
+	MovimentiRepoImpl movimentiRepository;
+	@Autowired
+	SaldiRepoImpl saldiRepository;
+	@Autowired
+	ContiRepoImpl contiRepository;
 	
 	public ContoModel findConto(String nConto) {
 		ContoModel contoCorrente=new ContoModel();
