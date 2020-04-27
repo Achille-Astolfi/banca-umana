13e23ee7a60c7b6d1f94b7a07cb727955cbf5583
Ayoub <Ayoub@example.com>
2020-04-27 17:03:55 +0200

    -

--- src/main/java/com/example/bancaumana/serviceimpl/MovimentiServiceImpl.java
+++ src/main/java/com/example/bancaumana/serviceimpl/MovimentiServiceImpl.java
@@ -1,10 +1,12 @@
 package com.example.bancaumana.serviceimpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.bancaumana.entity.Movimento;
 import com.example.bancaumana.model.MovimentoModel;
 import com.example.bancaumana.repoimpl.MovimentiRepoImpl;
 import com.example.bancaumana.repoimpl.StoriciRepoImpl;
@@ -21,6 +23,22 @@ public class MovimentiServiceImpl {
 	public List<MovimentoModel> getMovimenti(String conto){
 		
 		return null;
+	}
+	
+	public List<MovimentoModel> ConvertToListModel(List<Movimento> list)
+	{
+		List<MovimentoModel> lista =new ArrayList<>();
+		MovimentoModel mModel=new MovimentoModel();
+		for (Movimento m: list)
+		{
+			mModel.setData(m.getDataMovimento());
+			mModel.setDescrizione(m.getDescrizione());
+			mModel.setImporto(m.getImpMovimento());
+			lista.add(mModel);
+		}
+		if (lista)
+		return lista;
+		
 	}
 	
 	public List<MovimentoModel> getMovimentiStorico(String conto){
