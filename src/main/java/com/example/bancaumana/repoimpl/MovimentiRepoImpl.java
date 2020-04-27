eb0d9e46ea8d63c420eeeda4671769605229fe80
Ayoub <Ayoub@example.com>
2020-04-27 12:55:49 +0200

    .

--- src/main/java/com/example/bancaumana/repoimpl/MovimentiRepoImpl.java
+++ src/main/java/com/example/bancaumana/repoimpl/MovimentiRepoImpl.java
@@ -1,5 +1,7 @@
 package com.example.bancaumana.repoimpl;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 import javax.sql.DataSource;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Repository;
 
 import com.example.bancaumana.entity.Movimento;
 import com.example.bancaumana.mapper.MovimentoRowMapper;
+import com.example.bancaumana.util.Utilities;
 /**
  * 
  * @author Riccardo
@@ -27,6 +30,17 @@ public class MovimentiRepoImpl extends JdbcDaoSupport {
 		JdbcTemplate jdbcTemplate = this.getJdbcTemplate();
 		MovimentoRowMapper movimentoRowMapper = new MovimentoRowMapper();
 		List<Movimento> list = jdbcTemplate.query("", movimentoRowMapper, "", "");
+	}
+	
+	public BigDecimal sommaSaldo(String nConto, Date date) {
+		JdbcTemplate jdbcTemplate = this.getJdbcTemplate();
+		BigDecimal somma;
+		String data=Utilities.formatDateToString(date);
+		somma = jdbcTemplate.queryForObject("SELECT * SUM(COALESCE(mov_importomovimento, 0)) AS saldo"
+				+ "FROM movimenti\r\n" +  
+				"WHERE mov_datamovimento = ? AND mov_stato != 0\r\n" + 
+				"AND mov_numeroconto = ? \r\n", BigDecimal.class,nConto,data );
+		return somma;
 	}
 
 }
