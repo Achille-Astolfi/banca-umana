9292f571739085e71e5accd489080c917772c22b
Jan Licciardello <licciardelloj@example.com>
2020-04-27 10:38:02 +0200

    Jan

--- src/main/java/com/example/bancaumana/mapper/SaldoRowMapper.java
+++ src/main/java/com/example/bancaumana/mapper/SaldoRowMapper.java
@@ -1,23 +1,34 @@
 package com.example.bancaumana.mapper;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.springframework.jdbc.core.RowMapper;
 import com.example.bancaumana.entity.Conto;
 import com.example.bancaumana.entity.Saldo;
+
 /**
  * 
  * @author Stefano
+ * @author Jan
  *
  */
-public class SaldoRowMapper implements RowMapper<Saldo>{
-	
+public class SaldoRowMapper implements RowMapper<Saldo> {
+
 	@Override
 	public Saldo mapRow(ResultSet rs, int rowNum) throws SQLException {
 		Saldo saldo = new Saldo();
-		// BEGIN set dei valori
+		// BEGIN set dei valori   
 
+		saldo.setnConto(rs.getString("sal_numeroconto"));
+		saldo.setDataSaldo(rs.getDate("sal_datasaldo"));
+		saldo.setImpSaldo(rs.getBigDecimal("columnIndex"));
+		
 		// END set dei valori
 		return saldo;
 	}
+
 }
