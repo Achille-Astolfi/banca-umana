fd894e33f859356a2618deb1d8089585e23df49f
Jan Licciardello <licciardelloj@example.com>
2020-04-27 14:47:10 +0200

    Jan

--- src/main/java/com/example/bancaumana/model/MovimentoModel.java
+++ src/main/java/com/example/bancaumana/model/MovimentoModel.java
@@ -1,47 +1,49 @@
 package com.example.bancaumana.model;
-
+   
 import java.math.BigDecimal;
-import java.sql.Date;
+import java.util.Date;
 
 import org.springframework.hateoas.core.Relation;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
+/**
+ * 
+ * @author Jan
+ *
+ */
 @Relation(collectionRelation = "movimenti")
 public class MovimentoModel {
 
-	
-	@JsonProperty("movimenti")
-	String movimenti;
 	@JsonProperty("importo")
-	BigDecimal importo;
+	private BigDecimal importo;
 	@JsonProperty("data")
-	Date data;
+	@JsonFormat(pattern = "yyyy-MM-dd", shape = Shape.STRING)
+	private Date data;
 	@JsonProperty("descrizione")
-	String descrizione;
-	
-	
-	public String getMovimenti() {
-		return movimenti;
-	}
-	public void setMovimenti(String movimenti) {
-		this.movimenti = movimenti;
-	}
+	private String descrizione;
+
 	public BigDecimal getImporto() {
 		return importo;
 	}
+
 	public void setImporto(BigDecimal importo) {
 		this.importo = importo;
 	}
+
 	public Date getData() {
 		return data;
 	}
+
 	public void setData(Date data) {
 		this.data = data;
 	}
+
 	public String getDescrizione() {
 		return descrizione;
 	}
+
 	public void setDescrizione(String descrizione) {
 		this.descrizione = descrizione;
 	}
