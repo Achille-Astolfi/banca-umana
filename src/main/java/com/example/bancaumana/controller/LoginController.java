aaae68556790e027c16ee45f98c10fccb92ba68e
Achille Astolfi <astolfia@example.com>
2020-04-29 17:16:28 +0200

    CORS

--- src/main/java/com/example/bancaumana/controller/LoginController.java
+++ src/main/java/com/example/bancaumana/controller/LoginController.java
@@ -4,11 +4,13 @@ import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
 public class LoginController {
